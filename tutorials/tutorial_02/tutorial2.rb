9e5c0fd7efdf9515655760509bb5d133efc78a52
pps99 <99gautham@gmail.com>
2022-02-17 17:19:33 +0630

    feature/tutorial_02 is Updated

---
+++ tutorials/tutorial_02/tutorial2.rb
@@ -0,0 +1,19 @@
+require "date"
+
+def input
+  date = Date.today
+  puts "Put The DOB in the Format yyyy/mm/dd"
+  dob = gets.chomp
+  user_dob = Date.parse(dob)
+
+  if Date.valid_date? user_dob.year.to_i, user_dob.month.to_i, user_dob.day.to_i
+    user_age = date.year - user_dob.year
+    if user_age > 18
+      puts "#{user_age},adult"
+    else
+      puts "#{user_age},child"
+    end
+  end
+end
+
+input
