cd0d7158ded412bb5f34f9c5cb85219c32a2880d
pps99 <50921817+pps99@users.noreply.github.com>
2022-02-28 15:59:26 +0630

    "feature/tutorial_09 routes.rb is updated"

--- tutorials/tutorial9/config/routes.rb
+++ tutorials/tutorial9/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  root 'upload#index'
-  get "/upload", to: "upload#index"
-  get "/folder_handler", to: "upload#index"
-  post "/folder_handler", to: "upload#create_folder"
-  get "/return_root", to: "upload#return_root"
+  root "upload#index"
+  post "/create_folder", to: "upload#create_folder"
+  get "/create_folder", to: "upload#folder_handler"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
