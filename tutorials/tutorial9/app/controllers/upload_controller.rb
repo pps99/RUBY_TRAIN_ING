7aaecb7f73d3c6bd29307a88f2264439660f491e
pps99 <50921817+pps99@users.noreply.github.com>
2022-02-28 15:57:59 +0630

    "feature/tutorial_09 upload_contoller.rb is updated"

--- tutorials/tutorial9/app/controllers/upload_controller.rb
+++ tutorials/tutorial9/app/controllers/upload_controller.rb
@@ -1,29 +1,26 @@
 class UploadController < ApplicationController
-  require "fileutils"
-
   def create_folder
     @folder_name = params[:folder_name]
-    @file_name = params[:picture].original_filename
-    @arr = @file_name.split('.')
-    
-    if Dir.exists?("./app/assets/images/#{@folder_name}")
-      @error = "Directory Already Exists Cannot Create"
-      render "index"
+    if (params[:picture] == nil || @folder_name == nil)
+      redirect_to root_path, alert: "File cannot be empty"
     else
-      Dir.mkdir("./app/assets/images/#{@folder_name}")
-      if(@arr.last == "jpg" || @arr.last == "jpeg" || @arr.last == "png")
-      File.open("./app/assets/images/#{@folder_name}/#{@file_name}", "wb") do |f|
-        f.write params[:picture].read
-      end
-      render "folder_handler"
+      @file_name = params[:picture].original_filename
+      @arr = @file_name.split(".")
+      if (Dir.exists?("./app/assets/images/#{@folder_name}"))
+        redirect_to root_path, alert: "Folder already exits"
       else
-        @error = "Accepts only the jpg/jpeg/png"
-        render "index"
+        session[:folder_name] = @folder_name
+        session[:file_name] = @file_name
+        Dir.mkdir("./app/assets/images/#{@folder_name}")
+        if (@arr.last == "jpg" || @arr.last == "jpeg" || @arr.last == "png")
+          File.open("./app/assets/images/#{@folder_name}/#{@file_name}", "wb") do |f|
+            f.write params[:picture].read
+          end
+          render "folder_handler"
+        else
+          redirect_to root_path, alert: "Accepts only the jpg/jpeg/png"
+        end
       end
     end
-  end
-
-  def return_root
-    redirect_to "/"
   end
 end
