e9ec9322944b32a7a2112f4adabfc9eb5a3ae873
pps99 <50921817+pps99@users.noreply.github.com>
2022-02-28 16:05:09 +0630

    "feature/tutorial_08 routes.rb is updated"

--- tutorials/tutorial8/config/routes.rb
+++ tutorials/tutorial8/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "login#index"
-  get "/login", to: "login#index"
-  get "/handle_login", to: "login#index"
-  post "/handle_login", to: "login#handle_login"
-  get "/handle_logout", to: "login#handle_logout"
+  post "/handle_login", to: "login#validate_login"
+  get "/handle_login", to: "login#handle_login"
 end
