eabbef07d6f2df86603bfdcc9d0525a5d48314e2
pps99 <99gautham@gmail.com>
2022-03-01 14:55:39 +0630

    feature/tutorial_12 is added

---
+++ tutorials/tutorial12/db/migrate/20220301074945_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :address
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
